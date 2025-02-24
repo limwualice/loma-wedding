06a499c1370b2a0f80080e5fcd15a73c62bd9b12
Alice Lim <limwualice@gmail.com>
2025-02-23 17:41:30 -0800

    changed all images and all pages to fit new aesthetic

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: ["./*.{html,js}"], // This tells Tailwind to scan all HTML files in your project
+    theme: {
+        // fontFamily: {
+        //     display: ["Young Serif", "serif"],
+        // },
+    },
+    plugins: [],
+  };
+
